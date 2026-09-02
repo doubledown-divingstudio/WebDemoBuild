f16b272a25146a462c72e3b12c9684255d3317ac
broken-fire-egg <6716000@naver.com>
2026-09-02 14:06:57 +0900

    change mode mobile to desktop

--- Build/WebDemoBuild.loader.js
+++ Build/WebDemoBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56499597,
+    wasmFileSize: 56578765,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
